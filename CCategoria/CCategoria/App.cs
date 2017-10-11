2ffa5edfbced87cc9b0534f27c68030828c69021
erikmarto <dam2.emartorell@institutoserpis.org>
2017-10-11 09:59:43 +0200

    Añadido CCategoria

---
+++ CCategoria/CCategoria/App.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace CCategoria
+{
+    public class App
+    {
+        private IDbConnection connection;
+        public App()
+        {
+        }
+
+        static App() { 
+        }
+
+		private static App instance = new App();
+        //public static App GetInstance() {
+        //    return instance;
+        //}
+        public static App Instance {
+            get { return instance; }
+        }
+        //public IDbConnection GetConnection () {
+        //    return connection;
+        //}
+        //public void  SetConnection (IDbConnection connection){
+        //    this.connection = connection;
+        //}
+        public IDbConnection Connection {
+            get { return connection; }
+            set { connection = value; }
+        }
+    }
+}
