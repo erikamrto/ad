a5cd37bf91f118074611b8282e395438c458f073
erikmarto <dam2.emartorell@institutoserpis.org>
2017-10-25 10:31:18 +0200

    CCategoria refactorizada

--- CCategoria/CCategoria/CategoriaWindow.cs
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -8,8 +8,9 @@ namespace CCategoria
     public partial class CategoriaWindow : Gtk.Window
     {
         object id;
-        public CategoriaWindow(object id) : this()
-        {
+        public CategoriaWindow(object id) : base(Gtk.WindowType.Toplevel) {
+            this.Build();
+
             this.id = id;
             IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
             dbCommand.CommandText = "select * from categoria where id = @id";
@@ -19,16 +20,18 @@ namespace CCategoria
             string nombre = (string)dataReader["nombre"];
             dataReader.Close();
             entryNombre.Text = nombre;
+
+			saveAction.Activated += delegate {
+				update();
+				Destroy();
+			};
         }
 
-        public CategoriaWindow()  : base(Gtk.WindowType.Toplevel){
+        public CategoriaWindow()  : base(Gtk.WindowType.Toplevel) {
             this.Build();
 
             saveAction.Activated += delegate {
-                if (id == null)
-                    insert();
-                else
-                    update();
+                insert();
                 Destroy();
             };
         }
@@ -36,8 +39,9 @@ namespace CCategoria
         private void insert() {
 			string nombre = entryNombre.Text;
 			IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
-			dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+            dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
 			DbCommandHelper.AddParameter(dbCommand, "nombre", nombre);
+
 			dbCommand.ExecuteNonQuery();
         }
 
@@ -47,6 +51,7 @@ namespace CCategoria
 			dbCommand.CommandText = "update categoria set nombre=@nombre where id = @id";
 			DbCommandHelper.AddParameter(dbCommand, "id", id);
             DbCommandHelper.AddParameter(dbCommand, "nombre", nombre);
+
 			dbCommand.ExecuteNonQuery();
 		}
     }
