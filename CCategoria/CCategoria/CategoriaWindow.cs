3c5f02b178bed99e46ccdedec41351ebca90eaef
erikmarto <dam2.emartorell@institutoserpis.org>
2017-10-30 13:23:18 +0100

    CCategoría refactorizado

--- CCategoria/CCategoria/CategoriaWindow.cs
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -5,25 +5,10 @@ using Serpis.Ad;
 
 namespace CCategoria {
     public partial class CategoriaWindow : Gtk.Window {
-        object id;
-        public CategoriaWindow(object id) : base(Gtk.WindowType.Toplevel) {
+
+        public CategoriaWindow(Categoria categoria) : base(Gtk.WindowType.Toplevel) {
             this.Build();
-
-            this.id = id;
-
-            Categoria categoria = CategoriaDao.Load(id);
-            entryNombre.Text = categoria.Nombre;
-
-			saveAction.Activated += delegate {
-                categoria.Nombre = entryNombre.Text;
-                CategoriaDao.Save(categoria);
-				Destroy();
-			};
-        }
-
-        public CategoriaWindow()  : base(Gtk.WindowType.Toplevel) {
-            this.Build();
-            Categoria categoria = new Categoria();
+            entryNombre.Text = categoria.Nombre ?? "";
 
             saveAction.Activated += delegate {
                 categoria.Nombre = entryNombre.Text;
